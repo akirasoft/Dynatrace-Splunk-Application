3551c3bd813622e8ca0654bfb083303723c95b38
Mike Villiger <akirasoft@hotmail.com>
2014-12-30 10:38:47 -0600

    redirected some output to stdout
    redirected non-error output to stdout

--- src/APM_dynatrace/bin/runFlume.py
+++ src/APM_dynatrace/bin/runFlume.py
@@ -12,21 +12,19 @@ try:
 	if retcode < 0:
 		print >> sys.stderr, "Java not in Path", -retcode
 	else:
-		print >> sys.stderr, "Child returned", retcode
+		print >> sys.stdout, "Child returned", retcode
 except OSError as e:
     print >>sys.stderr, "Execution failed:", e
 
-# print >> sys.stderr, "Current Working directory:", os.getcwd()
 appdir = os.path.dirname(os.path.dirname(__file__))
 
 if not appdir:
     appdir = os.getcwd();
 else:
-    print >> sys.stderr, "App directory:", appdir
+    print >> sys.stdout, "App directory:", appdir
 
 
-# check current OS for process detection
-currentOS = os.name
+
 
 c1 = os.path.join(appdir, "bin", "apache-flume-1.3.1-bin", "lib", "*")
 c2 = os.path.join(appdir, "bin", "apache-flume-1.3.1-bin", "lib", "flume-ng-node-1.3.1.jar")
@@ -34,7 +32,7 @@ c3 = os.path.join(appdir, "bin", "dtFlume.jar")
 
 classpath = c1 + os.pathsep + c2 + os.pathsep + c3
 
-print >> sys.stderr, "Class path:", classpath
+print >> sys.stdout, "Class path:", classpath
 
 log4j = os.path.join(appdir,"bin", "apache-flume-1.3.1-bin", "conf", "log4j.properties")
 
@@ -42,20 +40,19 @@ flumeconf = os.path.join(appdir,"bin","flume-conf.properties");
 
 pidfilename = os.path.join(appdir, 'flume.pid')
 
-
-#print >> sys.stderr, "java  -Xmx20m -Dlog4j.configuration=file:" + log4j + " -cp " + classpath + " org.apache.flume.node.Application -f flume-conf.properties -n agent1"
-
 cmdline = "java  -Xmx20m -Dlog4j.configuration=file:" + log4j + " -cp " + classpath + " org.apache.flume.node.Application -f " + flumeconf + " -n agent1"
 
+# check current OS for process detection
+currentOS = os.name
+
+# OS detection drives process for detecting running Flume instances based on pid file	
 if currentOS == 'posix':
 	if os.access(pidfilename, os.F_OK):
 		flumeFilepid = int(open(pidfilename).read())
 		if os.path.exists("/proc/%s" % flumeFilepid): 
-			#print "Process already running as PID ", flumeFilepid
 			print >>sys.stderr, "Process already running as PID ", flumeFilepid
 			sys.exit(1)
 		else:
-			#print "PID file exists but process is no longer running. Removing pidfile"
 			print >>sys.stderr, "PID file exists but process is no longer running. Removing pidfile"
 			os.remove(pidfilename)
 	try:
@@ -69,7 +66,7 @@ if currentOS == 'posix':
 		if retcode < 0:
 			print >>sys.stderr, "Child was terminated by signal", -retcode
 		else:
-			print >>sys.stderr, "Child returned", retcode
+			print >>sys.stdout, "Child returned", retcode
 	except OSError as e:
 			print >>sys.stderr, "Execution failed:", e
 else:
@@ -92,9 +89,9 @@ else:
 				if retcode < 0:
 					print >>sys.stderr, "Child was terminated by signal", -retcode
 				else:
-					print >>sys.stderr, "Child returned", retcode
+					print >>sys.stdout, "Child returned", retcode
 			except OSError as e:
 				print >>sys.stderr, "Execution failed:", e	
 	else:
-		print "Unsupported OS"
+		print >>sys.stdout, "Unsupported OS"
 		sys.exit(1)
